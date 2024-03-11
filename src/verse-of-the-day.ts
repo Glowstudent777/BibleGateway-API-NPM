d3bee57270cfb3359e89d91db835e9c6203053bb
Glowstudent777 <cameronmenken7@gmail.com>
2024-03-11 16:57:54 -0500

    Little rewriting

--- src/verse-of-the-day.ts
+++ src/verse-of-the-day.ts
@@ -3,7 +3,9 @@ import * as cheerio from 'cheerio';
 
 export async function getVerseOfTheDay() {
     try {
-        const { data } = await axios.get("https://www.bible.com/verse-of-the-day");
+        const { data } = await axios.get("https://www.biblegateway.com/reading-plans/verse-of-the-day/next");
+        // Version add "?version=ASV" to the end of the URL to get the verse in a specific version
+        
         const $ = cheerio.load(data);
 
         const versesArray = $("p.text-gray-50").map((_, p) => $(p).text().replace(/\n/g, ' ')).get();
