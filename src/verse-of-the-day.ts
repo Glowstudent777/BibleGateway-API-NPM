0e544d655195cd054c4ebff78b42c7ce8c4eedfe
Glowstudent777 <cameronmenken7@gmail.com>
2024-03-11 17:50:19 -0500

    VOTD

--- src/verse-of-the-day.ts
+++ src/verse-of-the-day.ts
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-export async function getVerseOfTheDay() {
+export async function getVerseOfTheDay(version: string = "NLT") {
+    const baseURL = "https://www.biblegateway.com/reading-plans/verse-of-the-day/next";
+    let URL = `${baseURL}?version=${version}`;
     try {
-        const { data } = await axios.get("https://www.biblegateway.com/reading-plans/verse-of-the-day/next");
-        // Version add "?version=ASV" to the end of the URL to get the verse in a specific version
-        
+        const { data } = await axios.get(URL);
         const $ = cheerio.load(data);
 
-        const versesArray = $("p.text-gray-50").map((_, p) => $(p).text().replace(/\n/g, ' ')).get();
-        const citationsArray = $(".mbs-2").map((_, p) => $(p).text()).get();
+        let verse = $("#rp-passage-0-5121").text().split(' ').slice(1)[0];
+        let pass = $(".rp-passage-display").text();
 
-        return { citation: citationsArray[1], passage: versesArray[0] };
+        return {
+            citation: pass,
+            passage: verse
+        }
     } catch (err) {
         console.error(err);
     }
